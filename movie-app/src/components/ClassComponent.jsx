7e548ba00cb31bd7f8d2144aad61838f188a4144
Deepesh-Sabran <deepeshsabran@gmail.com>
2023-12-07 15:43:04 +0530

    react intro

---
+++ movie-app/src/components/ClassComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>class component !!</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default ClassComponent;
